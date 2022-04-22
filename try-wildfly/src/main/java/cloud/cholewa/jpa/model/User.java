e7ef1eddea268ff5b0275bd92b41d14857f97383
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-04-22 19:26:44 +0200

    [JAVA-START] persistence in Java EE8 and WildFly

---
+++ try-wildfly/src/main/java/cloud/cholewa/jpa/model/User.java
@@ -0,0 +1,25 @@
+package cloud.cholewa.jpa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+}
